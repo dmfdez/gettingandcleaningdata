a7c3e2c6284a6aea17791c5f9851dbb47527b276
dmfdez <dmfdez@gmail.com>
2014-09-18 12:46:12 +0200

    Download Data in R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,36 @@
-setwd('C:/Users/dmartin/Google Drive/Coursera/Getting and Cleaning Data/EjerciciosUCI HAR Dataset/')
+setwd("C:/Users/dmartin/Google Drive/Coursera/Getting and Cleaning Data/Ejercicios/")
 
+print("Download data and prepare tidy data set")
+
+url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+file <- 'dataset.zip'
+download(url,file)
+unzip(file)
+print("Dataset downloaded. Please, proceed with run.analysis()")
+
+# Create funtion load.dataset
+load.dataset <- function(type, selected.features, activity.labels){
+  
+  path <- paste(type, '/', sep = '')
+  feature.vectors.file <- paste(path, 'X_', type, '.txt', sep = '')
+  activity.labels.file <- paste(path, 'y_', type, '.txt', sep = '')
+  subject.ids.file <- paste(path, 'subject_', type, '.txt', sep= '')
+
+  # Load data files
+  feature.vectors.data <- read.table(feature.vectors.file)[,selected.features$id]
+  activity.labels.data <- read.table(activity.labels.file)[,1]
+  subject.ids.data <- read.table(subject.ids.file)[,1]
+  
+  # Name variables 
+  names(feature.vectors.data) <- selected.features$label
+  feature.vectors.data$label <- factor(activity.labels.data, levels=activity.labels$id, labels=activity.labels$label)
+  feature.vectors.data$subject <- factor(subject.ids.data)
+  
+  # Return processed dataset
+  feature.vectors.data
+}
+
+setwd("UCI HAR Dataset/")
 print("Read and process features and activity text files")
 
 # Load id->feature label data
@@ -41,10 +72,10 @@ setnames(tidy.dt, tidy.dt.names)
 
 # Save datasets
 setwd('..')
-write.csv(merged.dt, file = 'tidy.csv', row.names = FALSE)
+write.csv(merged.dt, file = 'tidy.txt', row.names = FALSE)
 write.csv(tidy.dt,
-file = 'tidy.csv',
+file = 'tidy.txt',
 row.names = FALSE, quote = FALSE)
 
 
-print("Finished processing. Tidy dataset is written to tidy.csv")
+print("Finished processing. Tidy dataset is written to tidy.txt")
